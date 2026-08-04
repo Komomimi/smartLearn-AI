234561221412f913cdb0a159b14cccc92490a715
Komomimi <672318530@qq.com>
2026-08-04 23:27:40 +0800

    fix: allow PUT method in CORS for settings save endpoint

--- smartlearn-backend/main.py
+++ smartlearn-backend/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
-    allow_methods=["GET", "POST", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Content-Type", "Accept"],
 )
 
